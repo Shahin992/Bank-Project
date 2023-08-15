474d92aa75f4c31237073eab2a02af70b6f0ed50
Shahin992 <shahinkhan5979@gmail.com>
2023-08-15 14:19:41 +0600

    final submission

--- js/deposite.js
+++ js/deposite.js
@@ -5,6 +5,19 @@ document.getElementById('deposit-btn').addEventListener('click',function(){
     const depositField = document.getElementById('deposit-input');
     const depositAmountString = depositField.value;
     const depositFinalAmout = parseFloat(depositAmountString);
+
+    if(depositFinalAmout <0){
+        alert('Please deposit more than 0 Dollar');
+        depositField.value = '';
+        return;
+        
+    }
+    if (isNaN(depositFinalAmout)){
+        alert('Please deposit a number');
+        depositField.value = '';
+        return;
+
+    }
    
    
     
@@ -22,7 +35,7 @@ document.getElementById('deposit-btn').addEventListener('click',function(){
     const newBalanceString = newBalance.innerText;
     const newBalanceFinal=parseFloat(newBalanceString);
 
-    const totalNewBanlance = totalDeposit + newBalanceFinal;
+    const totalNewBanlance = newBalanceFinal + depositFinalAmout;
     newBalance.innerText = totalNewBanlance;
 
 
--- js/withdraw.js
+++ js/withdraw.js
@@ -5,22 +5,54 @@ document.getElementById('withdraw-btn').addEventListener('click',function(){
     const withdrawString =withdrawInput.value;
     const withdrawInputFinal = parseFloat(withdrawString);
 
+    if(withdrawInputFinal < 0){
+        alert('Please Withdraw more than 0 Dollar');
+        withdrawInput.value = '';
+        return;
+    }
+
+
+
+    if (isNaN(withdrawInputFinal)){
+        alert('Please withdraw a number');
+        withdrawInput.value = '';
+        return;
+
+    }
+
+
+ 
+
     const previousWithdrawBalance = document.getElementById('withdraw-balance');
     const previousWithdrawString = previousWithdrawBalance.innerText;
     const previousWithdrawFinal = parseFloat(previousWithdrawString);
+
+    
+
+
+   
+
+    const newBalance = document.getElementById('new-balance')
+    const newBalanceString = newBalance.innerText;
+    const newBalanceFinal=parseFloat(newBalanceString);
+
+    if(withdrawInputFinal > newBalanceFinal){
+        alert("you can't withdraw more than your balance");
+        withdrawInput.value = '';
+        return;
+
+    }
 
     const totalWithdraw = withdrawInputFinal + previousWithdrawFinal;
     previousWithdrawBalance.innerText = totalWithdraw;
 
     withdrawInput.value = '';
 
-    const newBalance = document.getElementById('new-balance')
-    const newBalanceString = newBalance.innerText;
-    const newBalanceFinal=parseFloat(newBalanceString);
+  
+     const totalNewBanlance = newBalanceFinal - withdrawInputFinal;
+     newBalance.innerText = totalNewBanlance;
 
-    const totalNewBanlance = newBalanceFinal - totalWithdraw;
-    newBalance.innerText = totalNewBanlance;
-
+    
 
 
 })
